bdd9bc6aecf8a534b8f5eac1a805f125a077ae5a
Abdlrahman <abdlrahmansaber2@gmail.com>
2021-11-01 18:27:43 +0200

    Cleanup, install django enviro for environment setup

--- frontend/src/pages/Cities/All.js
+++ frontend/src/pages/Cities/All.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
